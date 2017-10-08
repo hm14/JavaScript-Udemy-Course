b002d093fc95ef0598da382225c59b8937d38521
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-08 19:20:19 -0400

    feat: added type coercion and variable mutation
    Added type coercion and variable mutation examples.

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var firstName = 'John';
 var lastName = 'Doe';
 var age = 23;
+
+// js uses type coercion so variable types are changed on the go
+// thus, in the code below age can be concatenated to a str
 var profile = firstName + ' ' + lastName + ' is ' + age;
 
 console.log(profile);
 
-var minor = false;
-
+var isMinor = false;
 console.log('Is ' + firstName + ' ' + lastName + ' a minor?')
-console.log(minor)
+console.log(isMinor)
+
+var noValue;
+console.log('Initially, noValue = ' + noValue)
+noValue = 'valuable'
+console.log('Then, noValue = ' + noValue)
+
+// variable mutation
+noValue = 'no longer valuable'
+console.log('Later, noValue = ' + noValue)
