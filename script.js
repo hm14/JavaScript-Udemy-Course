7fcf7209230fee93d9f23213346e8d75ac82fcab
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-08 20:33:49 -0400

    feat: added boolean operations

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 var firstName = 'John';
 var lastName = 'Doe';
 var age = 23;
+var isMinor = false;
+var isStudent = true;
+
+if (age < 18) {
+  isMinor = true
+}
 
 // js uses type coercion so variable types are changed on the go
 // thus, in the code below age can be concatenated to a str
 var profile = firstName + ' ' + lastName + ' is ' + age;
 
 console.log(profile);
-
-var isMinor = false;
 console.log('Is ' + firstName + ' ' + lastName + ' a minor?')
 console.log(isMinor)
 
-if(isMinor) {
-  console.log('A minor');
-} else {
-  console.log('Not a minor');
+if(isMinor && isStudent) {
+  console.log('A minor eligible for school lunch');
+} else if(isMinor && !isStudent) {
+    console.log('A minor not eligible for school lunch');
+} else if(!isMinor && isStudent) {
+    console.log('Not a minor but eligible for school lunch');
+}
+else {
+  console.log('Not a minor and not eligible for school lunch');
 }
