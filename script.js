6dac1f3ebc5e370f228b971ac044e57dee1cc9e9
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-08 21:53:05 -0400

    feat: added array related functions

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 var players = ['John', 'Jane', 'Joanne'];
+var scores = new Array()
 
 function getScore() {
     return Math.floor(Math.random() * 10);
 }
 
-function game(firstPlayer, secondPlayer, thirdPlayer){
-  firstScore = getScore()
-  secondScore = getScore()
-  thirdScore = getScore()
-  if (firstScore > secondScore && firstScore > thirdScore) {
-    console.log(firstPlayer + ' won with ' + firstScore + ' points!')
-    console.log(secondPlayer + ' scored ' + secondScore + ' points.')
-    console.log(thirdPlayer + ' scored ' + thirdScore + ' points.')
-  } else if (secondScore > firstScore && secondScore > thirdScore) {
-    console.log(secondPlayer + ' won with ' + secondScore + ' points!')
-    console.log(firstPlayer + ' scored ' + firstScore + ' points.')
-    console.log(thirdPlayer + ' scored ' + thirdScore + ' points.')
-  } else if (thirdScore > firstScore && thirdScore > secondScore){
-    console.log(thirdPlayer + ' won with ' + thirdScore + ' points!')
-    console.log(firstPlayer + ' scored ' + firstScore + ' points.')
-    console.log(secondPlayer + ' scored ' + secondScore + ' points.')
-  } else {
-    console.log("It's a tie!")
-    console.log("The players scored " + firstScore + ', ' + secondScore + ', ' + thirdScore + ' points!')
-  }
+function addScores() {
+  scores.push(getScore());
+  scores.unshift(getScore());
 }
 
-game(players[0], players[1], players[2]);
+function removeScores() {
+  scores.pop()
+  scores.shift()
+}
+
+console.log(scores);
+addScores();
+console.log(scores);
+addScores();
+console.log(scores);
+removeScores();
+console.log(scores);
+
+if (scores.indexOf(1) === -1) {
+  console.log('1 is not a score')
+} else {
+  console.log('1 is a score')
+}
