cecd19123b155bcf3d14eee8a003abd9914bce7f
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-10 15:16:49 -0400

    feat: added recursion
    Also, fixed formatting

--- script.js
+++ script.js
@@ -3,35 +3,37 @@ var scores = []
 
 function getScore() {
     return Math.floor(Math.random() * 10);
-}
+};
 
 function playGame() {
   scores = []
   for (var j=0; j<players.length; j++){
     scores.push(0)
     for (var i=1; i<=3; i++) {
-      score = getScore()
-      scores[j] += score
-      console.log('On turn ' + i + ' ' + players[j] + ' scored: ' + score)
+      score = getScore();
+      scores[j] += score;
+      console.log('On turn ' + i + ' ' + players[j] + ' scored: ' + score);
     }
-    console.log(players[j] + ' scored a total of ' + scores[j] + ' points!')
+    console.log(players[j] + ' scored a total of ' + scores[j] + ' points!');
   }
-}
+};
 
 function showWinner() {
-  playGame()
+  playGame();
   var max = scores.reduce(function(a, b) {
       return Math.max(a, b);
   });
   var index = scores.indexOf(max);
   scores.sort();
   if (scores[0] == scores[1]) {
-    console.log(" * * * * * It's a tie >< * * * * * ")
+    console.log(" * * * * * It's a tie >< * * * * * ");
+    console.log(" *** Players get ready to score again! ***");
+    showWinner();
   } else {
-    console.log('*********************************************')
+    console.log('*********************************************');
     console.log('*** ' + players[index] + ' won with the highest score of ' + max + ' ***');
-    console.log('*********************************************')
+    console.log('*********************************************');
   }
 }
 
-showWinner()
+showWinner();
