97d09a8334361933032ef138c1618fa4c9b01d37
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-10 16:00:58 -0400

    feat: added age solution

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-var players = ['John', 'Jane', 'Joan', 'Jean', 'Jolo']
-var scores = []
+var names = ['John', 'Jane', 'Joan', 'Jean', 'Jolo'];
+var birthYears = [1990, 1995, 2000, 2005, 2009];
+var ages = [];
+var minors = [];
+var currentYear = 2017;
 
-function getScore() {
-    return Math.floor(Math.random() * 10);
+function getAge(birthYear) {
+  return currentYear - birthYear;
 };
 
-function playGame() {
-  scores = []
-  for (var j=0; j<players.length; j++){
-    scores.push(0)
-    for (var i=1; i<=3; i++) {
-      score = getScore();
-      scores[j] += score;
-      console.log('On turn ' + i + ' ' + players[j] + ' scored: ' + score);
+function isMinor(age) {
+    if (age < 18) {
+      return true;
+    } else {
+      return false;
     }
-    console.log(players[j] + ' scored a total of ' + scores[j] + ' points!');
-  }
 };
 
-function showWinner() {
-  playGame();
-  var max = scores.reduce(function(a, b) {
-      return Math.max(a, b);
-  });
-  var index = scores.indexOf(max);
-  scores.sort();
-  if (scores[0] == scores[1]) {
-    console.log(" * * * * * It's a tie >< * * * * * ");
-    console.log(" *** Players get ready to score again! ***");
-    showWinner();
-  } else {
-    console.log('*********************************************');
-    console.log('*** ' + players[index] + ' won with the highest score of ' + max + ' ***');
-    console.log('*********************************************');
+function show() {
+  for (var i=0; i<names.length; i++) {
+    ages.push(getAge(birthYears[i]));
+    minors.push(isMinor(ages[i]));
+    if (minors[i] == true) {
+      console.log(names[i] + ' is a minor with age ' + ages[i])
+    } else {
+      console.log(names[i] + ' is not a minor with age ' + ages[i])
+    }
   }
 }
 
-showWinner();
+show();
