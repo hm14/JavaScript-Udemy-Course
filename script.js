2df3cb9c56ae3ba7d45828134dc01bab658243c9
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-23 15:28:31 -0400

    feat: IIFE
    Immediately-invoked function expression

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-// functions returning functions
+// IIFE: immediately invoked function expession
+// not reusable functions
+// good for data privacy and code modularity
 
-function getInterviewQuestion(job) {
-  if (job === 'Teacher') {
-    return function(name) {
-      console.log(name + ', what do you know about growth mindset?');
-    }
-  } else if (job === 'Designer') {
-    return function(name) {
-      console.log(name + ', what is your design philosophy?');
-    }
-  } else if (job === 'Web developer') {
-    return function(name) {
-      console.log(name + ', what is CSRF?');
-    }
-  } else {
-    return function(name) {
-      console.log('Hello, ' + name + '. What motivates you?')
-    }
-  }
-}
 
-var teacherQuestion = getInterviewQuestion('Teacher');
-teacherQuestion('John');
+(function () {
+  var score = Math.random() * 10;
+  console.log(score);
+})();
 
-var designerQuestion = getInterviewQuestion('Designer');
-designerQuestion('Jane');
-
-var developerQuestion = getInterviewQuestion('Web developer');
-developerQuestion('Joanne');
-
-getInterviewQuestion('Manager')('Mary');
+(function (luck) {
+  var score = Math.random() * 10;
+  console.log(score + ' ' + score * luck);
+})(2);
