a2ef7f5ad29232e37e2b1fea2f49bd5cf2bd923a
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-23 15:47:53 -0400

    feat: closures

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// IIFE: immediately invoked function expession
-// not reusable functions
-// good for data privacy and code modularity
+// Closures
+// An inner function can access the variables and parameters of its outer function
+// even after the outer function has returned
 
+var currentYear = 2017;
 
-(function () {
-  var score = Math.random() * 10;
-  console.log(score);
-})();
+function retirement(retirementAge) {
+  return function(yearOfBirth) {
+    var str = ' years left until retirement!'
+    var age = currentYear - yearOfBirth;
+    console.log(retirementAge - age + str);
+  }
+}
 
-(function (luck) {
-  var score = Math.random() * 10;
-  console.log(score + ' ' + score * luck);
-})(2);
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1969);
+retirementGermany(1969);
+retirementIceland(1969);
+
+retirement(65)(1959);
