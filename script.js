f17a0ba199a9b9a588cb5bd5bc04c5d3f8af1e77
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-23 11:53:25 -0400

    feat: added func to prototype property

--- script.js
+++ script.js
@@ -12,16 +12,18 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name,
   this.yearOfBirth = yearOfBirth,
   this.job = job
-  this.calculateAge = function() {
-    console.log(currentYear - yearOfBirth);
-  }
 };
+
+// attached function calculateAge to the Person's prototype property
+Person.prototype.calculateAge = function() {
+    console.log(currentYear - this.yearOfBirth);
+}
 
 // new creates a new empty object
 // then the constructor function is called
 // this in the constructor points to the new empty object
 var john = new Person('John', 1999, 'teacher');
-var jane = new Person('Jane', 1989, 'manager');
+var jane = new Person('Jane', 1988, 'manager');
 var mark = new Person('Mark', 2001, 'student');
 
 john.calculateAge();
